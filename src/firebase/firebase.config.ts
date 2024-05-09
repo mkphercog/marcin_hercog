def0aca44eb9e3a728540c898f8b6d0b4654b737
Marcin Hercog <mkp.hercog@gmail.com>
2024-05-09 18:42:09 +0200

    fix(general): firebase env

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,11 @@
+import type { FirebaseOptions } from 'firebase/app'
+
+export const firebaseConfig: FirebaseOptions = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET
+}
+
+export const collectionPath = import.meta.env.VITE_FIREBASE_COLLECTION_PATH
